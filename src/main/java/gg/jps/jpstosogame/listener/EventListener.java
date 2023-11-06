adade22d945f3d810b6fd235468561cfc6e6e2b6
oieii <lpondboy@gmail.com>
2023-11-06 14:50:49 +0900

    fix #10

--- src/main/java/gg/jps/jpstosogame/listener/EventListener.java
+++ src/main/java/gg/jps/jpstosogame/listener/EventListener.java
@@ -4,6 +4,9 @@ import gg.jps.jpstosogame.JpsTosoGame;
 import gg.jps.jpstosogame.game.TosoGame;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -45,6 +48,8 @@ public class EventListener implements Listener {
     @EventHandler
     private void on(BlockPlaceEvent event) {
         if (event.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+        final Block bottomBlock = event.getBlockPlaced().getRelative(BlockFace.DOWN);
+        if (bottomBlock.getType() == Material.POLISHED_ANDESITE && event.getBlock().getType() == Material.BLACKSTONE) return;
         event.setCancelled(true);
     }
 }
