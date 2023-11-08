cffdf6d06d794f3d7f8b5c9c326d5e917abe98de
oieii <lpondboy@gmail.com>
2023-11-08 15:34:21 +0900

    fix #27

--- src/main/java/gg/jps/jpstosogame/game/handler/WaitingHandler.java
+++ src/main/java/gg/jps/jpstosogame/game/handler/WaitingHandler.java
@@ -52,8 +52,8 @@ public class WaitingHandler extends Handler {
         if (clickedBlock == null || event.getHand() != EquipmentSlot.HAND) return;
 
         switch (clickedBlock.getType()) {
-            case EMERALD_BLOCK -> getGame().teleportPrisonLocation(player);
-            case REDSTONE_BLOCK -> getGame().teleportOpeningGameLocation(player);
+            case EMERALD_BLOCK -> getGame().teleportOpeningGameLocation(player);
+            case REDSTONE_BLOCK -> getGame().teleportPrisonLocation(player);
             case DIAMOND_BLOCK -> randomHunterChoose(player);
         }
     }
